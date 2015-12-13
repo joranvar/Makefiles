3288a98f40ce1f22c44ab34477ddbb336ce0aed0
joranvar <bart.post@gmail.com>
2015-12-13 15:37:06 +0100

    Add FSharp.mk

---
+++ FSharp.mk
@@ -0,0 +1,12 @@
+CC.fs ?= env fsharpc
+FSharp.Core.dll ?= /nix/store/9nvx5380w2md40yzr63hbyh22aafsw4j-fsharp-3.1.2.5/lib/mono/4.5/FSharp.Core.dll
+
+%.dll: %.fs
+	mkdir -p $(@D)
+	$(CC.fs) $(filter-out $<,$(filter %.fs,$^)) $< $(addprefix -r:,$(filter %.dll,$^)) -o $@ -a --nologo
+
+%.exe: %.fs
+	mkdir -p $(@D)
+	$(CC.fs) $(filter-out $<,$(filter %.fs,$^)) $< $(addprefix -r:,$(filter %.dll,$^)) -o $@ --nologo
+	if [ '$(filter %.dll,$^)x' != 'x' ]; then cp -u $(filter %.dll,$^) $(@D); fi
+	cp -u $(FSharp.Core.dll) $(@D)
