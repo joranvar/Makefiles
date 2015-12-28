3e3882903a81d153b0a580e359db4b2c7b7bbd02
joranvar <bart.post@gmail.com>
2015-12-28 06:12:23 +0100

    Reformat and add helpful phony targets

--- NuGet.mk
+++ NuGet.mk
@@ -6,6 +6,19 @@ TOOLSDIR ?= tools/
 
 NUGET = $(TOOLSDIR)nuget/NuGet.exe
 
+.PHONY: all
+all: install_nuget
+
+.PHONY: clean
+clean: nugetclean
+
+.PHONY: install_tools
+install_tools: install_nuget
+
+.PHONY: realclean
+realclean: clean_nuget
+
+.PHONY: install_nuget
 install_nuget: $(NUGET)
 
 $(NUGET): | $(dir $(NUGET))
@@ -15,8 +28,14 @@ $(NUGET): | $(dir $(NUGET))
 %.nupkg: PKGNAME = $(basename $(firstword $(subst /, ,$@)))
 %.nupkg: VERSION = $(basename $(word 2,$(subst /, ,$@)))
 %.nupkg: | $(NUGET)
-	[ -d $(NUGETDIR)$(PKGNAME) ] || $(MONO) $(NUGET) install $(PKGNAME) -ExcludeVersion -OutputDirectory $(NUGETDIR) -Verbosity quiet $(if $(VERSION),-Version $(VERSION))
+	[ -d $(NUGETDIR)$(PKGNAME) ] || \
+		$(MONO) $(NUGET) install $(PKGNAME) \
+		-ExcludeVersion \
+		-OutputDirectory $(NUGETDIR) \
+		-Verbosity quiet \
+		$(if $(VERSION),-Version $(VERSION))
 
+.PHONY: nugetclean
 nugetclean:
 	$(RM) -r $(NUGETDIR)
 
