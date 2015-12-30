5cb80b3b085b732cbdd87e9394c1b3c97b0e9b6c
joranvar <bart.post@gmail.com>
2015-12-30 22:02:02 +0100

    Allow for an $(OUTDIR) and .exe for final outputs

--- CSharp.mk
+++ CSharp.mk
@@ -1,7 +1,16 @@
 CC.cs ?= env mcs
 System.dll ?= $(wildcard /nix/store/q3yf6a3ysjv0zgad9v2rb3rvllhih8l6-mono-4.0.3.20/lib/mono/4.5/Facades/*.dll)
+OUTDIR ?= bin/
 
-%.dll: %.cs #$(System.dll)
+$(OUTDIR)%.dll: %.cs
+	echo $?
 	mkdir -p $(@D)
 	@$(CC.cs) $(filter %.cs,$^) $(addprefix -r:,$(filter %.dll,$^)) -out:$@ -t:library -pkg:dotnet
-#	@cp -u $(filter %.dll,$^) $(@D)
+	if [ '$(filter %.dll,$^)x' != 'x' ]; then cp -u $(filter %.dll,$^) $(@D); fi
+
+$(OUTDIR)%.exe: %.cs
+	echo $?
+	mkdir -p $(@D)
+	@$(CC.cs) $(filter %.cs,$^) $(addprefix -r:,$(filter %.dll,$^)) -out:$@ -pkg:dotnet
+	if [ '$(filter %.dll,$^)x' != 'x' ]; then cp -u $(filter %.dll,$^) $(@D); fi
+	@cp -u $(System.dll) $(@D)
