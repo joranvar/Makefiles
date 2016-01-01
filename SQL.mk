5802f1ae6a3efdf553fe85f771a9c1a5e773fb74
joranvar <bart.post@gmail.com>
2016-01-01 16:43:16 +0100

    Run sql tests against an expected output

--- SQL.mk
+++ SQL.mk
@@ -3,6 +3,7 @@ SQSH_FLAGS ?=
 SQSH_OUTPUT_FLAGS ?= -mcsv
 CC.sql ?= env sqsh
 OUTDIR ?= bin/
+TESTDIR ?= test/
 
 $(OUTDIR)%.db: DB_NAME = $(notdir $(basename $@))
 $(OUTDIR)%.db: create_%.sql
@@ -11,3 +12,22 @@ $(OUTDIR)%.db: create_%.sql
 	-$(CC.sql) $(SQSH_FLAGS) -C"DROP DATABASE $(DB_NAME);"
 	$(CC.sql) $(SQSH_FLAGS) -C"CREATE DATABASE $(DB_NAME);"
 	$(CC.sql) $(SQSH_FLAGS) $(SQSH_OUTPUT_FLAGS) -D$(DB_NAME) $(addprefix -i,$^) -e -o$@
+
+cat_combined_with_go = for i in $(1); do cat $$i; echo "go"; done
+
+$(TESTDIR)%.sql.success: DB_NAME   = $(firstword $(subst /, ,$(subst $(TESTDIR),,$@)))
+$(TESTDIR)%.sql.success: TEST_NAME = $(*F)
+$(TESTDIR)%.sql.success: SETUP     = $(wildcard $(<D)/$(TEST_NAME)_setup*.sql)
+$(TESTDIR)%.sql.success: CLEANUP   = $(wildcard $(<D)/$(TEST_NAME)_cleanup*.sql)
+$(TESTDIR)%.sql.success: EXPECT    = $(<D)/$(TEST_NAME)_expect.out
+$(TESTDIR)%.sql.success: %_run_test.sql
+	make $(OUTDIR)$(DB_NAME).db
+	@echo "Running test:" $(TEST_NAME)
+	@echo "Setup from files:" $(SETUP)
+	-@$(call cat_combined_with_go,$(SETUP)) | $(CC.sql) $(SQSH_FLAGS) -D$(DB_NAME) -mnone -Lsemicolon_hack=0
+	@echo "Running:" $(filter-out %run_test.sql,$^) $(filter %run_test.sql,$^)
+	-@$(call cat_combined_with_go,$(filter-out %run_test.sql,$^) $(filter %run_test.sql,$^)) \
+		| $(CC.sql) $(SQSH_FLAGS) $(SQSH_OUTPUT_FLAGS) -D$(DB_NAME) -e -Lsemicolon_hack=0 \
+		| diff - $(EXPECT) && touch $@
+	@echo "Cleanup from files:" $(CLEANUP)
+	-@$(call cat_combined_with_go,$(CLEANUP)) | $(CC.sql) $(SQSH_FLAGS) -D$(DB_NAME) -mnone -Lsemicolon_hack=0
