c29de7320f7dfc600ff10bea61697dcd97a4682e
joranvar <bart.post@gmail.com>
2016-01-07 07:44:51 +0100

    Test existence of file instead of dir before rm

--- MakeUtils.mk
+++ MakeUtils.mk
@@ -1,3 +1,3 @@
 ifndef MAKE_clean
- MAKE_clean = if [ "$(dir $(wildcard $(1)))x" != "x" ]; then rm -fr $(wildcard $(1)); fi; if [ "$(dir $(wildcard $(1)))x" != "x" ]; then rmdir -p --ignore-fail-on-non-empty $(dir $(wildcard $(1))); fi
+ MAKE_clean = if [ "$(wildcard $(1))x" != "x" ]; then rm -fr $(wildcard $(1)); fi; if [ "$(dir $(wildcard $(1)))x" != "x" ]; then rmdir -p --ignore-fail-on-non-empty $(dir $(wildcard $(1))); fi
 endif
